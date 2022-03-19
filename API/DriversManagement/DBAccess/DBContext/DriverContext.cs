5eb05560802e47879d399845377fb94f4edf088a
Ankit Soni <ankit.soni05@gmail.com>
2022-03-19 15:03:00 +0530

    Initial Commit

---
+++ API/DriversManagement/DBAccess/DBContext/DriverContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBAccess.DBContext
+{
+    public class DriverContext : IdentityDbContext
+    {
+        public DriverContext(DbContextOptions<DriverContext> options)
+           : base(options)
+        {
+
+        }
+    }
+}
