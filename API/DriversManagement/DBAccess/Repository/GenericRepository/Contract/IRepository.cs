df5df0de2006a056256d0187056c7dfe910cbd62
Ankit Soni <ankit.soni05@gmail.com>
2022-03-19 15:35:00 +0530

    Added generic repository (Repository Pattern)

---
+++ API/DriversManagement/DBAccess/Repository/GenericRepository/Contract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DBAccess.Repository.GenericRepository.Contract
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        //Getting Data
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity,bool>> predicate);
+
+        //Adding Data
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        //Removing Data
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
